ae6020a18918add4f803644cd3c84aa17bd7f977
Huax <chenhua.ho0529@gmail.com>
2023-06-24 23:34:46 +0800

    1a2b更新

--- JS/HW-1a2b/script.js
+++ JS/HW-1a2b/script.js
@@ -1,6 +1,7 @@
 let answer = '';
 let input = document.querySelector("input");
 let screen = document.querySelector(".screen");
+let count = 0;
 input.disabled = true;
 document.querySelectorAll(".btn-num").forEach(x => {
     x.disabled = true;
@@ -42,11 +43,14 @@ press.disabled = true;
 press.addEventListener('click', function () {
     win();
     input.value = '';
-    document.querySelectorAll('.btn-num').forEach(x=>{
+    document.querySelectorAll('.btn-num').forEach(x => {
         x.disabled = false;
     })
 })
 
+document.querySelector(".btn-tip").addEventListener('click',function(){
+    alert(`請輸入不重複的四位正數數字`);
+})
 
 function insertNum(a) {
     input.value += a.target.innerText;
@@ -70,7 +74,7 @@ function win() {
     let a = 0;
     let b = 0;
     inputNum = input.value.trim()
-    if (!isNaN(inputNum) && inputNum.length == 4) {
+    if (!isNaN(inputNum) && inputNum.length == 4 && !isRepeat(inputNum)) {
         for (let i = 0; i < answer.length; i++) {
             if (answer[i] == inputNum[i]) {
                 a++;
@@ -79,17 +83,36 @@ function win() {
                 b++;
             }
         }
-        screen.innerText += `${inputNum}:${a}A${b}B\n`
+        count ++;
+        screen.innerText += `第${count}次:${inputNum}:${a}A${b}B\n`
         if (a == 4) {
             screen.innerText += `恭喜答對,答案為${answer}\n`;
             alert(`恭喜答對,答案為${answer}`);
         }
     }
     else {
-        alert("請正確輸入4位數數字");
+        alert("請正確輸入4位不重複的數字");
     }
 }
 
 function clear() {
     input.value = '';
 }
+
+function isRepeat(num) {
+    let numArray = num.split('');
+    for (let i = 0; i < numArray.length; i++) {
+        for (let j = i + 1; j < numArray.length; j++) {
+            if (numArray[i] === numArray[j]) {
+                return true; 
+            }
+        }
+    }
+    return false; 
+}
+
+//查到更簡單檢查重複的寫法!! set會把重複數值自動刪除
+// function isRepeat(num) {
+//     const numSet = new Set(num.split(''));
+//     return numSet.size !== num.length;
+//  }
